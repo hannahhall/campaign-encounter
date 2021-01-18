e138aee8d1628257282633d25b572c469d39a3e6
Hannah Hall <hannahkhall13@gmail.com>
2021-01-18 14:49:57 -0600

    monster detail page

--- ghosts-of-saltmarsh/src/app/classes/monster/monster.ts
+++ ghosts-of-saltmarsh/src/app/classes/monster/monster.ts
@@ -1,9 +1,37 @@
 export class Monster {
+    [x: string]: any;
     index: string;
     name: string;
     
     constructor(props: any) {
         this.index = props.index;
         this.name = props.name;
+
+        this.size = props.size;
+        this.type = props.type;
+        this.subtype = props.subtype;
+        this.alignment = props.alignment;
+        this.armorClass = props.armor_class;
+        this.hitPoints = props.hit_points;
+        this.hitDice = props.hit_dice;
+        this.speed = props.speed;
+        this.strength = props.strength;
+        this.dexterity = props.dexterity;
+        this.constitution = props.constitution;
+        this.intelligence = props.intelligence;
+        this.wisdom = props.wisdom;
+        this.charisma = props.charisma;
+        this.proficiencies = props.proficiencies;
+        this.damageVulnerabilities = props.damage_vulnerabilities;
+        this.damageResistance = props.damage_resistance;
+        this.damageImmunities = props.damage_immunities;
+        this.conditionImmunities = props.condition_immunities;
+        this.senses = props.senses;
+        this.languages = props.languages;
+        this.challengeRating = props.challenge_rating;
+        this.specialAbilities = props.special_abilities;
+        this.actions = props.actions;
+        this.legendaryActions = props.legendary_actions;
+        this.reactions = props.reactions;
     }
 }
--- ghosts-of-saltmarsh/src/app/components/monster/monster-detail/monster-detail.component.ts
+++ ghosts-of-saltmarsh/src/app/components/monster/monster-detail/monster-detail.component.ts
@@ -1,4 +1,6 @@
+import { MonsterService } from './../../../services/monster/monster.service';
 import { Component, OnInit } from '@angular/core';
+import { Monster } from 'src/app/classes/monster/monster';
 
 @Component({
   selector: 'app-monster-detail',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monster-detail.component.scss']
 })
 export class MonsterDetailComponent implements OnInit {
+  monster$: any;
 
-  constructor() { }
+  constructor(private monsterService: MonsterService) { }
 
   ngOnInit(): void {
+    this.monster$ = this.monsterService.monster
   }
 
+  convertBreaks(text: string) {
+    return text.replace(/(\\r\\n)|([\r\n])/gmi, '<br/>');
+  }
 }
